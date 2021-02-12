984a8ab7caba3b4a1fbe379adb83a9bf2c1b7ae9
Sam D <samidiaz3@gmail.com>
2021-02-12 19:38:52 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -22,7 +22,7 @@ def remove_strawberry(contacts)
             data.delete_if {|ice_cream| ice_cream == "strawberry"}
           #binding.pry
           end
-        end        
+        end
       end
-    end  
+    end
 end
